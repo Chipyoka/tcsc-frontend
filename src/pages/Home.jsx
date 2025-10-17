d059f61e58df82d4b3d1e0a9ee9f3dedd6ad1079
Chipyoka <mwapechipyoka18@gmail.com>
2025-10-17 03:41:33 +0200

    Initial setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,11 @@ const Home = () => {
 
         <div>
             <h3>We're building something Great !</h3>
+            <p>This is great</p>
+            <button className="btn-primary-lg">Hello</button>
+            <button className="btn-primary-outlined-lg">Hello</button>
+            <button className="btn-primary-sm">Hello</button>
+            <button className="btn-primary-outlined-sm">Hello</button>
         </div>
 
         </>
